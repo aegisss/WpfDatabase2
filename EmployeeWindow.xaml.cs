1ae3429f781de6ffd2d557cc779fdab834f7b900
aegisss <113986232+aegisss@users.noreply.github.com>
2023-03-17 11:47:24 +0300

    Add files via upload

---
+++ EmployeeWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Database.MagazineDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Database
+{
+    public partial class EmployeeWindow : Window
+    {
+        EmployeeTableAdapter adapter1 = new EmployeeTableAdapter();
+        PostTableAdapter adapter2 = new PostTableAdapter();
+        RangTableAdapter adapter3 = new RangTableAdapter();
+        int FK1 = 0;
+        int FK2 = 0;
+        public EmployeeWindow()
+        {
+            InitializeComponent();
+            EmployeeDataGrid.ItemsSource = adapter1.GetData();
+            FKRef.ItemsSource = adapter3.GetData();
+            FKRef.DisplayMemberPath = "Title";
+            FKRef1.ItemsSource = adapter2.GetData();
+            FKRef1.DisplayMemberPath = "Post_code";
+        }
+
+
+        private void FKRef_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            object cell = (FKRef.SelectedItem as DataRowView).Row[0];
+            FK1 = (int)cell;
+
+        }
+
+        private void FKRef1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            object cell = (FKRef1.SelectedItem as DataRowView).Row[0];
+            FK2 = (int)cell;
+        }
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            adapter1.InsertEmployee(NameText.Text, NameText1.Text, NameText2.Text, NameText3.Text, FK1, FK2);
+            EmployeeDataGrid.ItemsSource = adapter1.GetData();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Database.MagazineDataSetTableAdapters;
+namespace Database
+{
+    public partial class MainWindow : Window
+    {
+        RangTableAdapter adapter = new RangTableAdapter();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            RangDataGrid.ItemsSource = adapter.GetData();
+        }
+//Добавление данных в таблицу
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            adapter.Insert1(NameTbx.Text, NameTbx1.Text, NameTbx2.Text);
+            RangDataGrid.ItemsSource = adapter.GetData();
+        }
+//Переход в другое окно
+        private void GoToNext_Click(object sender, RoutedEventArgs e)
+        {
+            EmployeeWindow window = new EmployeeWindow();
+            window.Show();
+            this.Close();
+        }
+    }
+}
